a8058176120becaa9100dd89381eb96f93392d3c
mohammedalith <mohammedalith713@gmail.com>
2026-03-02 11:48:28 +0530

    'url'

--- docs/tb/tb-config.js
+++ docs/tb/tb-config.js
@@ -1,6 +1,6 @@
 export const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:3000/api/issues'
-    : 'https://document-docsify.onrender.com/api/issues';
+    : 'https://document-in-docsify-1.onrender.com/api/issues';
 
 export const state = {
     issues: [],
